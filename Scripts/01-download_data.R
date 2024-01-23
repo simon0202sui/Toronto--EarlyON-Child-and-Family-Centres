eec513822abcc1218c79520ec1b025d0befe9012
Pengyu <pengyu.sui02@gmail.com>
2024-01-23 20:52:42 +0000

    download data

--- Scripts/01-download_data.R
+++ Scripts/01-download_data.R
@@ -1,12 +1,10 @@
-library(opendatatoronto)
 library(dplyr)
 library(readr)
-
+library(opendatatoronto)
 resources <- list_package_resources("9fcff3e1-3737-43cf-b410-05acd615e27b")
 datastore_resources <- filter(resources, tolower(format) %in% c('csv', 'geojson'))
 data <- filter(datastore_resources, row_number() == 1) %>% get_resource()
 head(data)
 write_csv(x = data, file = "inputs/data/unedited_data.csv")
 
-library(dplyr)
 
